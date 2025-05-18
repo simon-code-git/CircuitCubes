eac844745e8f6fc59f70dbfff93e3deb1a5595f5
Simon Wong <gilbertfinnagan@gmail.com>
2025-05-18 13:46:09 -0600

    Update CircuitCubes.py

--- CircuitCubes/CircuitCubes.py
+++ CircuitCubes/CircuitCubes.py
@@ -58,97 +58,98 @@ class Constants:
         return len(self.constantsList)
 
 class Cube:
+    __version__ = '1.1.1'
+    
     def __init__(self, **kwargs):
-        self.verbose = kwargs.get('verbose', False)
-        self.address = kwargs.get('address', '')
-        self.jupyter = kwargs.get('jupyter', False)
-        from IPython import get_ipython
-        self.jupyter = not(get_ipython() is None)
-        print(self.verbose * self.jupyter * '\nRunning in Jupyter notebook. ')
-        self.constants_class = Constants()
-        self.constants = self.constants_class.get_constant
-        self.connect(self.address)
-        self.constants_class.set_address(self.address)
-
-    async def async_connect(self, address):
         try: 
-            if address != '': 
-                print(f'\nConnecting to Circuit Cube with address {address}. ')
-                self.client = BleakClient(address)
-            else: 
-                print(self.verbose * '\nScanning for Circuit Cube. ')
-                scanner = BleakScanner()
-                await scanner.start()
-                device = await scanner.find_device_by_name('Tenka')
-                if device is None: 
-                    print('\nNo Circuit Cube device found. ')
-                    quit()
-                address = device.address
-                print(self.verbose * f'\nConnecting to Circuit Cube with address {address}. ')
-                self.client = BleakClient(address)
-            self.address = address
-            await self.client.connect()
+            self.verbose = kwargs.get('verbose', False)
+            self.address = kwargs.get('address', '')
+            self.jupyter = kwargs.get('jupyter', False)
+            from IPython import get_ipython
+            self.jupyter = not(get_ipython() is None)
+            print(self.verbose * self.jupyter * '\nRunning in Jupyter notebook. ')
+            self.constants_class = Constants()
+            self.constants = self.constants_class.get_constant
+            self.connect(self.address)
+            self.constants_class.set_address(self.address)
         except Exception as e: 
             print(f'\n{e}')
-            quit()
+
+    async def async_connect(self, address):
+        if address != '': 
+            print(f'\nConnecting to Circuit Cube with address {address}. ')
+            self.client = BleakClient(address)
+        else: 
+            print(self.verbose * '\nScanning for Circuit Cube. ')
+            scanner = BleakScanner()
+            await scanner.start()
+            device = await scanner.find_device_by_name('Tenka')
+            if device is None: 
+                print('\nNo Circuit Cube device found. ')
+            address = device.address
+            print(self.verbose * f'\nConnecting to Circuit Cube with address {address}. ')
+            self.client = BleakClient(address)
+        self.address = address
+        await self.client.connect()
 
     def connect(self, address=''): 
-        if self.jupyter: 
-            nest_asyncio.apply()
-            loop = asyncio.get_event_loop()
-            if loop.is_running(): 
-                task = asyncio.create_task(self.async_connect(address))
-                return loop.run_until_complete(asyncio.gather(task)) 
-        else: 
-            asyncio.run(self.async_connect(address))
+        try: 
+            if self.jupyter: 
+                nest_asyncio.apply()
+                loop = asyncio.get_event_loop()
+                if loop.is_running(): 
+                    task = asyncio.create_task(self.async_connect(address))
+                    return loop.run_until_complete(asyncio.gather(task)) 
+            else: 
+                asyncio.run(self.async_connect(address))
+        except Exception as e:
+            print(f'\n{e}')
 
     async def async_information(self): # Read some device information from known GATT characteristics. 
         print('\nDevice information: ')
-        try:
-            device_name = await self.client.read_gatt_char(self.constants(6))
-            device_name = device_name.decode('utf-8')
-            print(f'    Name: {device_name}. ')
+    
+        device_name = await self.client.read_gatt_char(self.constants(6))
+        device_name = device_name.decode('utf-8')
+        print(f'    Name: {device_name}. ')
 
-            device_appearance = await self.client.read_gatt_char(self.constants(7))
-            device_appearance = int.from_bytes(device_appearance, 'big')
-            print(f'    Appearance code: {device_appearance}. ')
+        device_appearance = await self.client.read_gatt_char(self.constants(7))
+        device_appearance = int.from_bytes(device_appearance, 'big')
+        print(f'    Appearance code: {device_appearance}. ')
 
-            serial_number = await self.client.read_gatt_char(self.constants(15))
-            serial_number = serial_number.decode('utf-8')
-            print(f'    Serial number: {serial_number}. ')
+        serial_number = await self.client.read_gatt_char(self.constants(15))
+        serial_number = serial_number.decode('utf-8')
+        print(f'    Serial number: {serial_number}. ')
 
-            firmware = await self.client.read_gatt_char(self.constants(16))
-            firmware = firmware.decode('utf-8')
-            print(f'    Firmware: {firmware}. ')
+        firmware = await self.client.read_gatt_char(self.constants(16))
+        firmware = firmware.decode('utf-8')
+        print(f'    Firmware: {firmware}. ')
 
-            hardware = await self.client.read_gatt_char(self.constants(17))
-            hardware = hardware.decode('utf-8')
-            print(f'    Hardware: {hardware}. ')
+        hardware = await self.client.read_gatt_char(self.constants(17))
+        hardware = hardware.decode('utf-8')
+        print(f'    Hardware: {hardware}. ')
 
-            software = await self.client.read_gatt_char(self.constants(18))
-            software = software.decode('utf-8')
-            print(f'    Software: {software}. ')
+        software = await self.client.read_gatt_char(self.constants(18))
+        software = software.decode('utf-8')
+        print(f'    Software: {software}. ')
 
-            TX = self.constants(2) 
-            RX = self.constants(3)
-            await self.client.write_gatt_char(TX, bytes('b', 'utf-8'))
-            voltage = await self.client.read_gatt_char(RX)
-            print(f'    Battery voltage: {voltage.decode("utf-8")}. ')
+        TX = self.constants(2) 
+        RX = self.constants(3)
+        await self.client.write_gatt_char(TX, bytes('b', 'utf-8'))
+        voltage = await self.client.read_gatt_char(RX)
+        print(f'    Battery voltage: {voltage.decode("utf-8")}. ')
+            
+    def information(self): 
+        try:  
+            if self.jupyter: 
+                nest_asyncio.apply()
+                loop = asyncio.get_event_loop()
+                if loop.is_running(): 
+                    task = asyncio.create_task(self.async_device_information())
+                    return loop.run_until_complete(asyncio.gather(task)) 
+            else: 
+                asyncio.run(self.async_device_information())
         except Exception as e: 
             print(f'\n{e}')
-            quit()
-            import traceback
-            traceback.print_exc()
-    
-    def information(self): 
-        if self.jupyter: 
-            nest_asyncio.apply()
-            loop = asyncio.get_event_loop()
-            if loop.is_running(): 
-                task = asyncio.create_task(self.async_device_information())
-                return loop.run_until_complete(asyncio.gather(task)) 
-        else: 
-            asyncio.run(self.async_device_information())
 
     def motor_command(self, letter, velocity): 
         if letter == 'A': 
@@ -179,14 +180,17 @@ class Cube:
             await self.client.write_gatt_char(TX, self.motor_command(letter, 0).encode())
 
     def run_motor(self, letter, velocity, time, **kwargs):
-        if self.jupyter: 
-            nest_asyncio.apply()
-            loop = asyncio.get_event_loop()
-            if loop.is_running(): 
-                task = asyncio.create_task(self.async_run_motor(letter, velocity, time, **kwargs))
-                return loop.run_until_complete(asyncio.gather(task))
-        else: 
-            asyncio.run(self.async_run_motor(letter, velocity, time, **kwargs))
+        try: 
+            if self.jupyter: 
+                nest_asyncio.apply()
+                loop = asyncio.get_event_loop()
+                if loop.is_running(): 
+                    task = asyncio.create_task(self.async_run_motor(letter, velocity, time, **kwargs))
+                    return loop.run_until_complete(asyncio.gather(task))
+            else: 
+                asyncio.run(self.async_run_motor(letter, velocity, time, **kwargs))
+        except Exception as e: 
+            print(f'\n{e}')
 
     async def async_run_motors(self, letters, velocities, time, **kwargs): 
         smooth = kwargs.get('smooth', False)
@@ -201,14 +205,17 @@ class Cube:
                 await self.client.write_gatt_char(TX, command.encode())
 
     def run_motors(self, letters, velocities, time, **kwargs):  
-        if self.jupyter: 
-            nest_asyncio.apply()
-            loop = asyncio.get_event_loop()
-            if loop.is_running(): 
-                task = asyncio.create_task(self.async_run_motors(letters, velocities, time, **kwargs))
-                return loop.run_until_complete(asyncio.gather(task))
-        else: 
-            asyncio.run(self.async_run_motors(letters, velocities, time, **kwargs))
+        try: 
+            if self.jupyter: 
+                nest_asyncio.apply()
+                loop = asyncio.get_event_loop()
+                if loop.is_running(): 
+                    task = asyncio.create_task(self.async_run_motors(letters, velocities, time, **kwargs))
+                    return loop.run_until_complete(asyncio.gather(task))
+            else: 
+                asyncio.run(self.async_run_motors(letters, velocities, time, **kwargs))
+        except Exception as e: 
+            print(f'\n{e}')
 
     async def async_halt(self): 
         print('\nStopping all motors. ')
@@ -218,31 +225,39 @@ class Cube:
             await self.client.write_gatt_char(TX, command.encode())
 
     def halt(self): 
-        if self.jupyter: 
-            nest_asyncio.apply()
-            loop = asyncio.get_event_loop()
-            if loop.is_running(): 
-                task = asyncio.create_task(self.async_halt())
-                return loop.run_until_complete(asyncio.gather(task))
-        else: 
-            asyncio.run(self.async_halt())
+        try: 
+            if self.jupyter: 
+                nest_asyncio.apply()
+                loop = asyncio.get_event_loop()
+                if loop.is_running(): 
+                    task = asyncio.create_task(self.async_halt())
+                    return loop.run_until_complete(asyncio.gather(task))
+            else: 
+                asyncio.run(self.async_halt())
+        except Exception as e: 
+            print(f'\n{e}')
 
     def disconnect(self): 
-        if self.jupyter: 
-            print(self.verbose * '\nDisconnecting Circuit Cube. ')
-            nest_asyncio.apply()
-            loop = asyncio.get_event_loop()
-            if loop.is_running(): 
-                task = asyncio.create_task(self.client.disconnect())
-                return loop.run_until_complete(asyncio.gather(task))
-        else: 
-            print(self.verbose * '\nDisconnecting Circuit Cube. ')
-            quit()
+        try: 
+            if self.jupyter: 
+                print(self.verbose * '\nDisconnecting Circuit Cube. ')
+                nest_asyncio.apply()
+                loop = asyncio.get_event_loop()
+                if loop.is_running(): 
+                    task = asyncio.create_task(self.client.disconnect())
+                    return loop.run_until_complete(asyncio.gather(task))
+            else: 
+                print(self.verbose * '\nDisconnecting Circuit Cube. ')
+        except Exception as e: 
+            print(f'\n{e}')
 
     def get_constant(self, index): 
-        print(self.verbose * self.constants(index))
-        return self.constants(index)
-    
+        try: 
+            print(self.verbose * self.constants(index))
+            return self.constants(index)
+        except Exception as e: 
+            print(f'\n{e}')
+        
     def help(self):
         print('\n Visit https://github.com/simon-code-git/circuitcubes. ')
 
@@ -251,14 +266,18 @@ class Cube:
         RX = self.constants(3)
         await self.client.write_gatt_char(TX, bytes('b', 'utf-8'))
         voltage = await self.client.read_gatt_char(RX)
-        print(f'    Battery voltage: {voltage.decode("utf-8")}. ')
+        self.voltage = voltage.decode('utf-8')
+        print(f'\nBattery voltage is {voltage.decode("utf-8")}. ')
 
     def battery(self): 
-        if self.jupyter: 
-            nest_asyncio.apply()
-            loop = asyncio.get_event_loop()
-            if loop.is_running(): 
-                task = asyncio.create_task(self.async_battery())
-                return loop.run_until_complete(asyncio.gather(task))
-        else: 
-            asyncio.run(self.async_battery())
+        try: 
+            if self.jupyter: 
+                nest_asyncio.apply()
+                loop = asyncio.get_event_loop()
+                if loop.is_running(): 
+                    task = asyncio.create_task(self.async_battery())
+                    return loop.run_until_complete(asyncio.gather(task))
+            else: 
+                asyncio.run(self.async_battery())
+        except Exception as e: 
+            print(f'\n{e}')
